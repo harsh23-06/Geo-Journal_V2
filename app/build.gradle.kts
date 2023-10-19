f9347467449241df128a496b6edee61116da63f8
tanmays62343 <tanmays62343@gmail.com>
2023-10-19 13:05:15 +0530

    Initial Commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,70 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")  //KSP plugin
+}
+
+android {
+    namespace = "com.trx"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.trx"
+        minSdk = 24
+        //noinspection EditedTargetSdkVersion
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+
+    //Basics
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    //Maps and Places(place autocomplete search bar) Dependencies
+    implementation ("com.google.android.libraries.places:places:3.2.0")         //for autocomplete search
+    implementation ("com.google.android.gms:play-services-maps:18.2.0")         //for map
+    implementation ("com.google.android.gms:play-services-location:21.0.1")     //for current location
+
+    //Room database dependencies
+    val roomVersion = "2.6.0"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    ksp("androidx.room:room-compiler:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+
+    //coroutines :
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+    //To load and display image
+    implementation ("com.squareup.picasso:picasso:2.8")
+
+    //Testing
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
---
+++ app/src/main/java/com/trx/models/PlaceModel.kt
@@ -0,0 +1,20 @@
+package com.trx.models
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+@Entity(tableName = "Places")
+data class PlaceModel(
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+
+    val title : String,
+    val category : String,
+    val date : String,
+    val location : String,
+    val latitude : Double,
+    val longitude : Double
+
+) : Serializable
